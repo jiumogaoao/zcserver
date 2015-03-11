13c7101fb07e184190003869f86ceb69a9be8b94
jiumogaoao <394127821@qq.com>
2015-03-11 16:04:34 +0800

    announcement
    announcement

--- announcement.js
+++ announcement.js
@@ -10,5 +10,43 @@ if(socket){
 	 	}		
 };
 
+function add(socket,data,fn){
+	console.log("announcement/add");
+	var result={code:1};
+if(socket){
+	 	socket.emit("announcement_add",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function edit(socket,data,fn){
+	console.log("announcement/edit");
+	var result={code:1};
+if(socket){
+	 	socket.emit("announcement_edit",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function remove(socket,data,fn){
+	console.log("announcement/remove");
+	var result={code:1};
+if(socket){
+	 	socket.emit("announcement_remove",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
 
 exports.get=get;
+exports.add=add;
+exports.edit=edit;
+exports.remove=remove;
