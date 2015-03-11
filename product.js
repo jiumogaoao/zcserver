07f93dfa146cd1a195c9a2cbbb73778575eb0fb6
jiumogaoao <394127821@qq.com>
2015-03-11 15:42:04 +0800

    product
    product

--- product.js
+++ product.js
@@ -10,5 +10,43 @@ if(socket){
 	 	}		
 };
 
+function add(socket,data,fn){
+	console.log("product/add");
+	var result={code:1};
+if(socket){
+	 	socket.emit("product_add",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function edit(socket,data,fn){
+	console.log("product/edit");
+	var result={code:1};
+if(socket){
+	 	socket.emit("product_edit",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function remove(socket,data,fn){
+	console.log("product/remove");
+	var result={code:1};
+if(socket){
+	 	socket.emit("product_remove",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
 
 exports.get=get;
+exports.add=add;
+exports.edit=edit;
+exports.remove=remove;
