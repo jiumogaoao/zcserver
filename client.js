5412bf711b2d808f1d56f47c010bc86b5d41f6a6
jiumogaoao <394127821@qq.com>
2015-03-11 15:37:27 +0800

    client
    client

--- client.js
+++ client.js
@@ -1,4 +1,40 @@
 
+function checkUser(socket,data,fn){
+	console.log("client/checkUser");
+	var result={code:1};
+if(socket){
+	 	socket.emit("client_checkUser",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function checkPhone(socket,data,fn){
+	console.log("client/checkPhone");
+	var result={code:1};
+if(socket){
+	 	socket.emit("client_checkPhone",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function checkEmail(socket,data,fn){
+	console.log("client/checkEmail");
+	var result={code:1};
+if(socket){
+	 	socket.emit("client_checkEmail",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
 function login(socket,data,fn){
 	console.log("client/login");
 	var result={code:1};
@@ -11,7 +47,35 @@ if(socket){
 	 	}		
 };
 
+function register(socket,data,fn){
+	console.log("client/register");
+	var result={code:1};
+if(socket){
+	 	socket.emit("client_register",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
 
+function resetKey(socket,data,fn){
+	console.log("client/resetKey");
+	var result={code:1};
+if(socket){
+	 	socket.emit("client_resetKey",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+exports.checkUser=checkUser;
+exports.checkPhone=checkPhone;
+exports.checkEmail=checkEmail;
 exports.login=login;
+exports.register=register;
+exports.resetKey=resetKey;
 
 
