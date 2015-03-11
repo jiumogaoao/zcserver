bb4307fec241a5506fd84a163becb5bb0cf92da2
jiumogaoao <394127821@qq.com>
2015-03-11 15:52:56 +0800

    admin
    admin

--- admin.js
+++ admin.js
@@ -10,5 +10,43 @@ if(socket){
 	 	}		
 };
 
+function add(socket,data,fn){
+	console.log("admin/add");
+	var result={code:1};
+if(socket){
+	 	socket.emit("admin_add",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function edit(socket,data,fn){
+	console.log("admin/edit");
+	var result={code:1};
+if(socket){
+	 	socket.emit("admin_edit",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
+
+function remove(socket,data,fn){
+	console.log("admin/remove");
+	var result={code:1};
+if(socket){
+	 	socket.emit("admin_remove",result);
+	 }
+	 	else if(fn){
+	 		var returnString = JSON.stringify(result);
+	 		fn(returnString);
+	 	}		
+};
 
 exports.get=get;
+exports.add=add;
+exports.edit=edit;
+exports.remove=remove;
